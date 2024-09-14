9ae07ad2e2f1ebac08bfc05f91edb58a96971452
Ruben Bresler (Arch) <111847+rubenqba@users.noreply.github.com>
2024-09-13 19:47:30 -0600

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ export default async function Home() {
   const { configs } = await getIntegrations();
   return (
     <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <li className="col-span-1 divide-y divide-gray-200 rounded-xl bg-white shadow">
-        {configs.map((i) => (
-          <NangoIntegrationCard key={i.unique_key} integration={i} className="mt-5"/>
-        ))}
-      </li>
+      {configs.map((i) => (
+        <NangoIntegrationCard
+          key={i.unique_key}
+          integration={i}
+          className="mt-5"
+        />
+      ))}
     </div>
   );
 }
